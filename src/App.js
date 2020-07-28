05ae164ad8e447c73eafac86486a0db417b59326
Clara Mendes <claraplanejamento@gmail.com>
2020-07-27 23:59:47 -0300

    Adds components to main page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,39 @@ function App() {
     <div>
       <Menu />
 
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Javidx9 answer this little question."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
