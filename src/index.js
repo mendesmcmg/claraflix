9fe7d7886a2fdde422e17437f7db2a4d66727ac1
Clara Mendes <claraplanejamento@gmail.com>
2020-08-04 14:04:04 -0300

    adds categories

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import './index.css';
 import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
 
 const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-
       <Route exact path="/" component={Home} />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
